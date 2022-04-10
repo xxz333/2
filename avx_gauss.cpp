adcc0786387a4ccb69d92dd91d80953fffbd71c6
xxz333 <91120503+xxz333@users.noreply.github.com>
2022-04-11 04:24:33 +0800

    avx指令集

--- avx_gauss.cpp
+++ avx_gauss.cpp
@@ -28,8 +28,6 @@ void m_reset(float **m,int N)
                         for(int j=0;j<N;j++)
                                 m[i][j]+=m[k][j];
 }
-
-//普通高斯消元
 void gauss(float **A,int n)
 {
         for(int k=0;k<n;k++)
@@ -77,7 +75,6 @@ int main()
         int N;
 	cin>>N;
 	float** m;
-//        float m[500][500];
         for(int i=0;i<N;i++)
                 m=new float*[N];
         for(int i=0;i<N;i++)
@@ -85,18 +82,10 @@ int main()
         int n;//执行次数
         m_reset(m,N);//测试用例生成
         clock_t start,finish;
-//      QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
-//      QueryPerformanceCounter((LARGE_INTEGER *)&head);
-//      int ret=gettimeofday(&val,NULL);
         start=clock();
         for(int i=0;i<n;i++)
            gauss(m,N);
         finish=clock();
         cout<<n<<":"<<(finish-start)/float(CLOCKS_PER_SEC)<<endl;
-//      QueryPerformanceCounter((LARGE_INTEGER *)&tail);
-//      cout<<(tail-head)*1000.0 / freq<<"ms"<<endl;
-//      ret=gettimeofday(&newval,NULL);
-//      cout<<"diff:sec---"<<newval.tv_sec-val.tv_sec<<" microsec---"<<newval.tv_usec
-//             -val.tv_usec<<endl;
         return 0;
 }
